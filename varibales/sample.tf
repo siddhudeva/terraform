c5b1fd8cf57bf196e95131df59e31de89ef6f5c8
siddhu <siddhudangeti@gmail.com>
2022-01-15 08:30:19 +0530

    terraform variable examples

--- varibales/sample.tf
+++ varibales/sample.tf
@@ -1,9 +1,9 @@
 variable "new1" {
   description = "this is to specify the variables"
-  default = welcome
+  default = "welcome"
   type = string
 }
 
 output "new1" {
-  value = "var.new1"
+  value = var.new1
 }
