3f965fa86a9c4fa881a76842135bac34a9409401
siddhu <siddhudangeti@gmail.com>
2022-01-15 08:57:40 +0530

    terraform variable examples

--- varibales/sample.tf
+++ varibales/sample.tf
@@ -12,31 +12,40 @@
 #output "new2" {
 #  value = var.new2
 #}
+#// this is for varible types
+#variable "new3" {
+#  description = "This is for different variable types"
+#  default = "welcome to world"
+#  type = string
+#}
+#
+#variable "new4" {
+#  description = "This is for specifying the boolean"
+#  default = true
+#  type = bool
+#}
+#
+#variable "new5" {
+#  description = "this is for specifying the numbers in datatypes"
+#  default = 2300
+#  type = number
+#}
+#output "new3" {
+#  value = var.new3
+#}
+#output "new4" {
+#  value = var.new5
+#}
+#output "new5" {
+#  value = var.new4
+#}
 
-variable "new3" {
-  description = "This is for different variable types"
-  default = "welcome to world"
-  type = string
-}
 
-variable "new4" {
-  description = "This is for specifying the boolean"
-  default = true
-  type = bool
+// varibles lists
+variable "new6" {
+  description = "This is the variable list"
+  default = ["abc", "hello_world", "1000"]
 }
-
-variable "new5" {
-  description = "this is for specifying the numbers in datatypes"
-  default = 2300
-  type = number
+output "new6" {
+  value = var.new6[0]
 }
-output "new3" {
-  value = var.new3
-}
-output "new4" {
-  value = var.new5
-}
-output "new5" {
-  value = var.new4
-}
-
