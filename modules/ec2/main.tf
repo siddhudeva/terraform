cae7e5f4b262698797bf66c23db8ecd4f3fab1dc
siddhu <siddhudangeti@gmail.com>
2022-01-17 20:05:45 +0530

    terraform Module creation

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "terraform" {
-  ami           = "ami-0d652ece237863287"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = var.sg_id
+  ami                    = "ami-0d652ece237863287"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [var.sg_id]
 }
 
 variable "sg_id" {}
