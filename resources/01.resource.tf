9d6d96546346a7a9f90d09d1e076770304e29d08
siddhu <siddhudangeti@gmail.com>
2022-01-17 07:55:07 +0530

    terraform resource and provider file to create security group and instance

--- resources/01.resource.tf
+++ resources/01.resource.tf
@@ -1,7 +1,32 @@
-resource "aws_instance" "terraform" {
+resource "aws_instance" "terraform1" {
   ami = "ami-0760b951ddb0c20c9"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 }
 output "public_ip" {
-  value = aws_instance.terraform.public_ip
+  value = aws_instance.terraform1.public_ip
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh"
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
