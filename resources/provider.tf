b6993a2e622a58934f86a9fbebafd247a1b0c0b9
siddhu <siddhudangeti@gmail.com>
2022-01-17 09:11:19 +0530

    terraform resource and provider file to create security group and instance

--- resources/provider.tf
+++ resources/provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "terraformbucket020"
+    key    = "store/terraform.statefile"
+    region = "us-east-1"
+  }
+}
